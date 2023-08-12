9f6cc73803721d1522dd91f937a2a8a024060d8f
Skank16 <scampo.andrea@gmail.com>
2023-08-12 15:15:29 +0200

    Iced GUI

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,87 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use iced::executor;
+use iced::widget::{button, column, image, text};
+use iced::window;
+use iced::{Alignment, Application, Command, Element, Settings, Theme};
+use screenshots::Screen;
+use std::fs;
 
-use eframe::egui::{self, ColorImage};
-
-fn main() -> Result<(), eframe::Error> {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        ..Default::default()
-    };
-    eframe::run_native(
-        "Take screenshots and display with eframe/egui",
-        options,
-        Box::new(|_cc| Box::<MyApp>::default()),
-    )
+pub fn main() -> iced::Result {
+    Counter::run(Settings {
+        window: window::Settings {
+            size: (700, 600),
+            ..window::Settings::default()
+        },
+        ..Settings::default()
+    })
 }
 
-#[derive(Default)]
-struct MyApp {
-    continuously_take_screenshots: bool,
-    texture: Option<egui::TextureHandle>,
-    screenshot: Option<ColorImage>,
-    save_to_file: bool,
+#[derive(Debug)]
+struct Counter {
+    value: i32,
+    image: Option<image::Handle>,
 }
 
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            if let Some(screenshot) = self.screenshot.take() {
-                self.texture = Some(ui.ctx().load_texture(
-                    "screenshot",
-                    screenshot,
-                    Default::default(),
-                ));
-            }
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    TakeScreenshotPressed,
+    TakeScreenshot,
+}
 
-            ui.horizontal(|ui| {
-                ui.checkbox(
-                    &mut self.continuously_take_screenshots,
-                    "continuously take screenshots",
-                );
+impl Application for Counter {
+    type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
 
-                if ui.button("save to 'top_left.png'").clicked() {
-                    self.save_to_file = true;
-                    frame.request_screenshot();
-                }
-
-                ui.with_layout(egui::Layout::top_down(egui::Align::RIGHT), |ui| {
-                    if self.continuously_take_screenshots {
-                        if ui
-                            .add(egui::Label::new("hover me!").sense(egui::Sense::hover()))
-                            .hovered()
-                        {
-                            ctx.set_visuals(egui::Visuals::dark());
-                        } else {
-                            ctx.set_visuals(egui::Visuals::light());
-                        };
-                        frame.request_screenshot();
-                    } else if ui.button("take screenshot!").clicked() {
-                        frame.request_screenshot();
-                    }
-                });
-            });
-
-            if let Some(texture) = self.texture.as_ref() {
-                ui.image(texture, ui.available_size());
-            } else {
-                ui.spinner();
-            }
-
-            ctx.request_repaint();
-        });
+    fn new(_flags: ()) -> (Counter, Command<Message>) {
+        (
+            Counter {
+                value: 0,
+                image: None,
+            },
+            Command::none(),
+        )
     }
 
-    fn post_rendering(&mut self, _window_size: [u32; 2], frame: &eframe::Frame) {
-        if let Some(screenshot) = frame.screenshot() {
-            if self.save_to_file {
-                let pixels_per_point = frame.info().native_pixels_per_point;
-                let region =
-                    egui::Rect::from_two_pos(egui::Pos2::ZERO, egui::Pos2 { x: 100., y: 100. });
-                let top_left_corner = screenshot.region(&region, pixels_per_point);
-                image::save_buffer(
-                    "top_left.png",
-                    top_left_corner.as_raw(),
-                    top_left_corner.width() as u32,
-                    top_left_corner.height() as u32,
-                    image::ColorType::Rgba8,
-                )
-                .unwrap();
-                self.save_to_file = false;
+    fn title(&self) -> String {
+        String::from("Screenshot")
+    }
+
+    fn update(&mut self, message: Message) -> Command<Message> {
+        match message {
+            Message::TakeScreenshotPressed => Command::batch(vec![
+                window::change_mode(window::Mode::Hidden),
+                Command::perform(async {}, |()| Message::TakeScreenshot),
+            ]),
+            Message::TakeScreenshot => {
+                self.value += 1;
+                let screen = Screen::all().unwrap()[0];
+                let image = screen.capture().unwrap();
+                let filename = format!("screen_{}.png", self.value);
+                let buffer = image.to_png(None).unwrap();
+                fs::write(&filename, buffer).unwrap();
+                self.image = Some(image::Handle::from_path(&filename));
+                window::change_mode(window::Mode::Windowed)
             }
-            self.screenshot = Some(screenshot);
         }
+    }
+
+    fn view(&self) -> Element<Message> {
+        let image_viewer = self
+            .image
+            .as_ref()
+            .map(|image_handle| image::viewer(image_handle.clone()).into());
+
+        let image_viewer_element: Element<Message> =
+            image_viewer.unwrap_or_else(|| text::Text::new("No screenshot taken yet").into());
+
+        column![
+            button("Take Screenshot").on_press(Message::TakeScreenshotPressed),
+            text(self.value).size(50),
+            image_viewer_element,
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
     }
 }
