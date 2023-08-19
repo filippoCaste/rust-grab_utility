e6efca26f72d26440d773687ce0115e83e9eec3a
Filippo Castellarin <s308692@studenti.polito.it>
2023-08-19 10:56:53 +0200

    threads

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use eframe::egui::{self, Image};
+use eframe::egui::{self};
 use native_dialog::FileDialog;
 use screenshots::Screen;
 use std::fs;
 
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions {
-        fullscreen: true,
+        maximized: true,
         decorated: true,
         transparent: true,
         resizable: false,
@@ -19,14 +19,12 @@ fn main() -> Result<(), eframe::Error> {
     )
 }
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 struct MyApp {
-    image: Option<Image>,
-    buffer: Option<Vec<u8>>,
-    screen_rect: RectangleCrop
+    screen_rect: RectangleCrop,
 }
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 struct RectangleCrop {
     x_left: f32,
     y_left: f32,
@@ -35,46 +33,59 @@ struct RectangleCrop {
 }
 
 impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::Window::new("Screenshot").show(ctx, |ui| {
             if ui.button("Take screenshot").clicked() {
-                                ui.set_visible(false);
-                let screen = Screen::all().unwrap()[0];
-                let image = screen.capture_area(self.screen_rect.x_left.floor() as i32, self.screen_rect.y_left.floor() as i32, 
-                                                        self.screen_rect.width.floor() as u32, self.screen_rect.height.floor() as u32).unwrap();
-                self.buffer = Some(image.to_png(None).unwrap());
-                let result = FileDialog::new()
-                    .add_filter("PNG Image", &["png"])
-                    .add_filter("JPEG Image", &["jpg", "jpeg"])
-                    .add_filter("GIF Image", &["gif"])
-                    .show_save_single_file()
-                    .unwrap();
-                match result {
-                    Some(result) => {
-                        fs::write(result.clone(), self.buffer.clone().unwrap()).unwrap();
-                    }
-                    None => (),
-                };
-                ui.set_visible(true);
+                frame.set_visible(false);
+                
+                let coord = self.screen_rect.clone();
+                let t = std::thread::spawn(move || {
+                    let screen = Screen::all().unwrap()[0];
+                    let image = screen.capture_area(coord.x_left.floor() as i32, coord.y_left.floor() as i32, 
+                                                            coord.width.floor() as u32, coord.height.floor() as u32).unwrap();
+                    let buffer = Some(image.to_png(None).unwrap());
+                    let result = FileDialog::new()
+                        .add_filter("PNG Image", &["png"])
+                        .add_filter("JPEG Image", &["jpg", "jpeg"])
+                        .add_filter("GIF Image", &["gif"])
+                        .show_save_single_file()
+                        .unwrap();
+                    match result {
+                        Some(result) => {
+                            fs::write(result.clone(), buffer.clone().unwrap()).unwrap();
+                        }
+                        None => (),
+                    };
+                });
+
+                t.join().unwrap();
+                frame.set_visible(true);
             }
             if ui.button("Whole screen").clicked() {
-                ui.set_visible(false);
-                let screen = Screen::all().unwrap()[0];
-                let image = screen.capture().unwrap();
-                self.buffer = Some(image.to_png(None).unwrap());
-                let result = FileDialog::new()
-                    .add_filter("PNG Image", &["png"])
-                    .add_filter("JPEG Image", &["jpg", "jpeg"])
-                    .add_filter("GIF Image", &["gif"])
-                    .show_save_single_file()
-                    .unwrap();
-                match result {
-                    Some(result) => {
-                        fs::write(result.clone(), self.buffer.clone().unwrap()).unwrap();
-                    }
-                    None => (),
-                };
-                ui.set_visible(true);
+
+                frame.set_visible(false);
+
+                let t = std::thread::spawn(move || {
+                    let screen = Screen::all().unwrap()[0];
+                    let image = screen.capture().unwrap();
+                    let buffer = Some(image.to_png(None).unwrap());
+                    let result = FileDialog::new()
+                        .add_filter("PNG Image", &["png"])
+                        .add_filter("JPEG Image", &["jpg", "jpeg"])
+                        .add_filter("GIF Image", &["gif"])
+                        .show_save_single_file()
+                        .unwrap();
+                    match result {
+                        Some(result) => {
+                            fs::write(result.clone(), buffer.clone().unwrap()).unwrap();
+                        }
+                        None => (),
+                    };
+                });
+
+                t.join().and_then(|()| Ok({frame.set_visible(true);})).unwrap();
+                // frame.set_visible(true);
+
             }
         });
         let w = egui::Window::new("ciao")
